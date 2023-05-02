26acd8a5ae8ed990af5f606f74bbc18f32af50d5
Rahmonjon22 <rahmonjon2@yahoo.com>
2023-05-02 11:52:07 +0200

    php revisit

---
+++ 01_output.php
@@ -0,0 +1,27 @@
+<?php
+// echo 'hi';
+// echo 123,  'hi',  10.5;
+// print 12344  // works like echo but can only take in single argument
+// print_r([1,2,3]);
+// print_r([1, 2, 3, 4]); //  print single values and arrays
+// echo [1, 2, 3 ,4 ,5];
+// var_dump(true); //  returns more info like data type and length
+var_export(123) // similar to var_dump() outputs a string representation of variable
+?>
+
+
+<!-- // echo output strtings, numbers html etc  -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<h1><?php echo 'Post one' ?></h1>
+
+</body>
+</html>
---
+++ 02_output.php
@@ -0,0 +1,27 @@
+<?php
+/** PHP DATA TYPES */
+/*
+- String - A string is a series of characters surrounded by quotes
+- Integer - Whole numbers
+- Float - Decimal numbers
+- Boolean - true or false
+- Array - An array is a special variable, which can hold more than one value
+- Object - A class
+- NULL - Empty variable
+- Resource - A special variable that holds a resource
+*/
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<h1><?php echo 'Post one' ?></h1>
+
+</body>
+</html>
